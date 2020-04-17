1e99d25cc8b463fd4772eead1629894b6c652740
Emmanuel Buah <emmanuelbuah@gmail.com>
2020-04-16 22:52:33 -0500

    refactor: final tweaks to eslint + prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
   rules: {
     'no-unused-vars': 'off',
